b195f9d9859bed1b139d0fb663c5c29cdaa243dc
Jason Ou <catopia.app@gmail.com>
2021-05-23 12:27:12 +0800

    Refactor unit test code and add maximum value test.

--- BMI Calculator PracticeTests/BMI_Calculator_PracticeTests.swift
+++ BMI Calculator PracticeTests/BMI_Calculator_PracticeTests.swift
@@ -11,13 +11,15 @@ import XCTest
 
 class BMI_Calculator_PracticeTests: XCTestCase {
     
-    var textField: CustomTextField!
     var homeVC: HomeViewController!
+    var textField: CustomTextField!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
         textField = CustomTextField()
         homeVC = HomeViewController()
+        homeVC.loadView()
+        textField.delegate = homeVC
     }
 
     override func tearDownWithError() throws {
@@ -35,20 +37,42 @@ class BMI_Calculator_PracticeTests: XCTestCase {
         }
     }
     
-    func testAutoZeroCharAddition() {
-        homeVC.loadView()
-        textField.delegate = homeVC
-        
-        textField.text = ""
-        guard let shouldChangeCharacters = textField.delegate?.textField?(textField, shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: ".") else {
-            print("Failed to get text field delegate assertion")
-            return
+    func testAutoAdditionOfZeroBeforeDot() {
+        for _ in 1..<100 {
+            textField.text = ""
+            
+            guard let shouldChangeCharacters = textField.delegate?.textField?(textField, shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: ".") else {
+                print("Failed to get the status of textField's delegation")
+                return
+            }
+            if shouldChangeCharacters {
+                textField.text!.append(".")
+            }
+            
+            XCTAssert(textField.text == "0" + ".")
         }
-        
-        XCTAssert(shouldChangeCharacters)
-        textField.text! += "."
-        
-        XCTAssert(textField.text == "0.")
+    }
+    
+    func testValueIsOnlyWithinThreeDigits() {
+        for _ in 0..<100 {
+            textField.text = ""
+            
+            for _ in 0..<6 {
+                let randomCharacter = String(Int.random(in: 0...9))
+                let inputLocation = textField.text!.count
+                
+                guard let shouldChangeCharacter = textField.delegate?.textField?(textField, shouldChangeCharactersIn: NSRange(location: inputLocation, length: 0), replacementString: randomCharacter) else {
+                    print("Failed to get the status of textField's delegation")
+                    break
+                }
+                
+                if shouldChangeCharacter {
+                    textField.text?.append(randomCharacter)
+                }
+            }
+            
+            XCTAssert(Double(textField.text!)! < 1000)
+        }
     }
     
     //    func testPerformanceExample() throws {
@@ -59,3 +83,4 @@ class BMI_Calculator_PracticeTests: XCTestCase {
 //    }
 
 }
+
